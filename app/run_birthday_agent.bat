5673d518d8a04ca93d14f0aa8946d426f035e2a4
Yashovardhan <vegitomui2004@gmail.com>
2026-03-06 11:28:44 +0530

    BAT file added

---
+++ app/run_birthday_agent.bat
@@ -0,0 +1,4 @@
+@echo off
+cd C:\Users\Yashovardhan\PyCharmMiscProject\birthday-whatsapp-ai
+call venv\Scripts\activate
+python -m app.scheduler
--- app/test_whatsapp_sender.py
+++
@@ -1,9 +0,0 @@
-from app.whatsapp_sender import send_whatsapp_message
-
-if __name__ == "__main__":
-
-    phone = "918826215970"
-
-    message = "Happy Birthday! 🎉 Have an amazing day!"
-
-    send_whatsapp_message(phone, message)
